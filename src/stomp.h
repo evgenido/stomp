2d952d351ca9dc1d1df59579b8346c6466055275
evgenido <evgeni_dobrev@developer.bg>
2013-01-26 21:32:55 +0100

    Updated the header documentation.

--- src/stomp.h
+++ src/stomp.h
@@ -8,8 +8,6 @@ extern "C" {
 #endif
 
 /**
- * @typedef stomp_session_t
- *
  * An opaque STOMP sesstion handle
  *
  * @see stomp_session_new()
@@ -17,21 +15,40 @@ extern "C" {
  */
 typedef struct _stomp_session stomp_session_t;
 
+/**
+ * Structure representing a STOMP header entry
+ *
+ * All functions which accept headers as a parameter use this, and 
+ * also all context structures.
+ */
 typedef struct {
 	const char *key; /**< null terminated string */
 	const char *val; /**< null terminated string */
 } stomp_hdr_t;
 
+/**
+ * This structure is provided to the client code
+ * which registered a callback for SCB_CONNECTED.
+ */
 typedef struct {
 	size_t hdrc; /**< number of headers */
 	const stomp_hdr_t *hdrs; /**< pointer to an array of headers */
 } stomp_ctx_connected_t;
 
+/**
+ * This structure is provided to the client code
+ * which registered a callback for SCB_RECEIPT.
+ */
 typedef struct {
 	size_t hdrc; /**< number of headers */
 	const stomp_hdr_t *hdrs; /**< pointer to an array of headers */
 } stomp_ctx_receipt_t;
 
+
+/**
+ * This structure is provided to the client code
+ * which registered a callback for SCB_ERROR.
+ */
 typedef struct { 
 	size_t hdrc; /**< number of headers */
 	const stomp_hdr_t *hdrs; /**< pointer to an array of headers */
@@ -39,6 +56,10 @@ typedef struct {
 	size_t body_len; /**< length of body in bytes */
 } stomp_ctx_error_t;
 
+/**
+ * This structure is provided to the client code
+ * which registered a callback for SCB_MESSAGE.
+ */
 typedef struct {
 	size_t hdrc; /**< number of headers */
 	const stomp_hdr_t *hdrs; /**< pointer to an array of headers */
@@ -48,15 +69,44 @@ typedef struct {
 
 typedef void(* stomp_callback_t)(stomp_session_t *s, void *callback_ctx, void *session_ctx);
 
+/**
+ * List of events the client code can register 
+ * a callback for.
+ *
+ * Aside from the server responses the client
+ * can also register for a user callback (SCB_USER). The callback
+ * will be called within every itteration of stomp_run(). 
+ * The amount of time between the calls is set to 1 second when no 
+ * heart-beat header is provided. Otherwise it wil get called 
+ * with the smallest timeneeded to satisfy the requuired heart-beats.
+ *
+ * @seen stomp_callback_set
+ * @seen stomp_callback_del
+ */
 typedef enum {
-	SCB_CONNECTED,
-	SCB_ERROR,
-	SCB_MESSAGE,
-	SCB_RECEIPT,
-	SCB_USER
+	SCB_CONNECTED, /**< server sended CONNECTED */
+	SCB_ERROR, /**< server sended ERROR */
+	SCB_MESSAGE, /**< server sended MESSAGE  */
+	SCB_RECEIPT, /**< server sended RECEIPT  */
+	SCB_USER /**< user slot */
 } stomp_cb_type_t;
 
+/**
+ * Register a callback when a particular event occurs.
+ *
+ * @param s pointer to a session handle
+ * @param type type of event to register for
+ * @param cb callback to register
+ */
 void stomp_callback_set(stomp_session_t *s, stomp_cb_type_t type, stomp_callback_t cb);
+
+/**
+ * Delete callback for a particular event.
+ *
+ * @param s Pointer to a session handle
+ * @param type Type of event to register for
+ * @param cb Callback to register
+ */
 void stomp_callback_del(stomp_session_t *s, stomp_cb_type_t type);
 
 /**
@@ -64,7 +114,7 @@ void stomp_callback_del(stomp_session_t *s, stomp_cb_type_t type);
  *
  * @param callbacks Callbacks to run when a certain STOMP event occurs.
  * @param session_ctx A data pointer to pass to the callback.
- * @return a newly allocated session or @c NULL on errors.
+ * @return a newly allocated session or NULL on errors.
  *
  * @see stomp_session_free()
  */
@@ -83,25 +133,27 @@ void stomp_session_free(stomp_session_t *s);
  * Connect to a STOMP broker.
  *
  * Headers parameter MUST contain the headers required by the specification.
+ * Note that in order to get notified of the server responses you must
+ * register the appropriate handler and call stomp_run()
  *
- * @param s pointer to a session handle
- * @param host
- * @param service
- * @param hdrc
- * @param hdrs
+ * @param s Pointer to a session handle.
+ * @param host Hostname to connect to.
+ * @param service Service of port to connect to.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
  *
- * @return 0 on success; negative on error and errno is set.
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_connect(stomp_session_t *s, const char *host, const char *service, int hdrc, const stomp_hdr_t *hdrs);
 
 /**
  * Disconnect from a STOMP broker.
  *
- * @param s pointer to a session handle
- * @param hdrc
- * @param hdrs
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
  *
- * @return 0 on success; negative on error and errno is set.
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_disconnect(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -112,7 +164,16 @@ int stomp_disconnect(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  * Headers SHOULD contain an "id" header with unique key.
  * Headers MAY contan an "ack" header. If none is provided "ack:auto" will be sent.
  *
- * If no "id" header is provided one will be generated. The return value is the key.
+ * If no "id" header is provided one will be generated. The return value is the key, and it must be 
+ * fed to stomp_unsibsribe().
+ *
+ * Note that if an "id" header si provided no attempt will be made
+ * to assure the uniqueness of the value.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
  * @return negative on error; or client_id to be used in stomp_unsubscribe()
 */
 int stomp_subscribe(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
@@ -123,6 +184,12 @@ int stomp_subscribe(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  * Headers MUST contain a "destination" header key.
  * For Stomp 1.1+, "id" header key per the specifications.  
  * client_id is the handle returned by stomp_subscribe()
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_unsubscribe(stomp_session_t *s, int client_id, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -131,6 +198,12 @@ int stomp_unsubscribe(stomp_session_t *s, int client_id, int hdrc, const stomp_h
  *
  * Headers MUST contain a "transaction" header key 
  * with a value that is not an empty string.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_begin(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -139,6 +212,12 @@ int stomp_begin(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  *
  * Headers MUST contain a "transaction" header key 
  * with a value that is not an empty string.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_abort(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -148,6 +227,12 @@ int stomp_abort(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  * Stomp 1.0 Headers MUST contain a "message-id" header key.
  * Stomp 1.1 Headers must contain a "message-id" key and a "subscription" header key.
  * Stomp 1.2 Headers must contain a unique "id" header key.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_ack(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -157,6 +242,12 @@ int stomp_ack(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  * Stomp 1.1 Headers must contain a "message-id" key and a "subscription" header key.
  * Stomp 1.2 Headers must contain a unique "id" header key.
  * Disallowed for an established STOMP 1.0 connection.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_nack(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -165,6 +256,12 @@ int stomp_nack(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  *
  * Headers MUST contain a "transaction" header key 
  * with a value that is not an empty string.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_commit(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
 
@@ -174,9 +271,27 @@ int stomp_commit(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs);
  * Headers MUST contain a "destination" header key.
  * Headers SHOULD contain a "content-type" header key.
  * Header "content-length" will be set according to body_len parameter.
+ *
+ * @param s Pointer to a session handle.
+ * @param hdrc Number of STOMP headers.
+ * @param hdrs Pointer to an array of STOMP headers.
+ * @param body Pointer to the message body.
+ * @param body_len Length of the body in bytes.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately.
  */
 int stomp_send(stomp_session_t *s, int hdrc, const stomp_hdr_t *hdrs, void *body, size_t body_len);
 
+/**
+ * Runs the library main loop.
+ * 
+ * This function will not return until either he server closes the 
+ * connection, or the client calls stomp_disconnect()
+ *
+ * @param s Pointer to a session handle.
+ *
+ * @return 0 on success; negative on error and errno is set appropriately
+ */
 int stomp_run(stomp_session_t *s);
 
 #ifdef __cplusplus
