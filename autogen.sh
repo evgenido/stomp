466fc428dc0a4e07d61188bb10b7f4158bcef026
evgenido <evgeni_dobrev@developer.bg>
2013-01-30 15:29:09 +0100

    Initialize m4

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,6 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+mkdir -p m4
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
